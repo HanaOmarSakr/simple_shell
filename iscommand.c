842c318a80714fb4afab6ffbd3e6efa629016fa2
Hana Sakr <hana.omar.sakr@gmail.com>
2023-09-21 04:53:59 +0300

    iscommand

--- iscommand.c
+++ iscommand.c
@@ -2,7 +2,7 @@
 
 /**
  * iscommand - ........
- * @path: ............
+ * @filepath: ............
  * Return: ..........
 */
 int iscommand(char *filepath)
@@ -11,13 +11,13 @@ int iscommand(char *filepath)
 
 	if (filepath == NULL || stat(filepath, &file_stat) != 0)
 	{
-		return 0;
+		return (0);
 	}
 
 	if ((file_stat.st_mode & S_IFMT) == S_IFREG)
 	{
-		return 1;
+		return (1);
 	}
 
-	return 0;
+	return (0);
 }
